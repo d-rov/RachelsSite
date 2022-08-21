3effb69538748239dbffeddae6c46084aa4c2d48
d-rov <duncan.rov@gmail.com>
2022-08-21 15:38:12 -0600

    Initial commit.

---
+++ rachel-client/src/components/Landing.js
@@ -0,0 +1,20 @@
+import './Landing.css'
+
+function Landing() {
+    return (
+      <div className="Landing">
+        <div className="Symbol">
+            <h1>Symbol</h1>
+        </div>
+        <div className="Navbar">
+            <h1>Navbar</h1>
+        </div>
+        <div className="Showcase">
+            <h1>Hello World!</h1>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Landing;
+  
