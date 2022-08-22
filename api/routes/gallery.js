7d6e1be649c7665bb3a649f270b5562e4168586c
d-rov <duncan.rov@gmail.com>
2022-08-22 15:52:06 -0600

    Added routes to gallery router.

--- api/routes/gallery.js
+++ api/routes/gallery.js
@@ -3,7 +3,19 @@ const router = express.Router()
 
 router.get("/", (req, res) => {
     res.json({
-        message: "This is the gallery page."
+        message: "This is the gallery page. This endpoint will get all images"
+    })
+})
+
+router.get("/getImageById", (req, res) => {
+    res.json({
+        message: "This endpoint gets images by ID."
+    })
+})
+
+router.get("/getImagesByMedium", (req, res) => {
+    res.json({
+        message: "This endpoint get images by media type."
     })
 })
 
