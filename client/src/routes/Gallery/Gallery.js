c5d631bf99953881868e52cd72f5a0f824412ed9
d-rov <duncan.rov@gmail.com>
2022-08-22 23:37:34 -0600

    Working on image styling.

--- client/src/routes/Gallery/Gallery.js
+++ client/src/routes/Gallery/Gallery.js
@@ -13,17 +13,21 @@ import './Gallery.css'
 
 function Gallery() {
   let thought = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam pellentesque nec nam aliquam sem et. Elit ullamcorper dignissim cras tincidunt lobortis feugiat. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed. Quam lacus suspendisse faucibus interdum posuere. Aliquet sagittis id consectetur purus ut. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Et molestie ac feugiat sed lectus vestibulum mattis. Interdum varius sit amet mattis vulputate enim. Sem integer vitae justo eget."
-  const [data, setData] = useState(null)
+  const [data, setData] = useState([])
 
   useEffect(() => {
     fetch("/gallery")
       .then((res) => res.json())
-      .then((data) => setData(data.message[0]))
-  })
+      .then((data) => setData(data.message))
+  }, [])
 
   return (
     <div className="Gallery">
       <p>{!data ? "Loading..." : data.name}</p>
+      {data.map((entry) => (
+        //
+        <h1 key={entry.name} >{!entry ? "Loading..." : entry.name}</h1>
+      ))}
       <Link className="home__link" to="/">Home</Link>
       <div className="artcase">
         <Artwork image={ohm} text={"this is image 1"} title={"image 1"} thoughts={thought} />
