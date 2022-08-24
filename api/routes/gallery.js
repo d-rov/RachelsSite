9c4add6dcf807640fd8342e3f5c5e56a16171edf
d-rov <duncan.rov@gmail.com>
2022-08-23 20:13:18 -0600

    Added /addArtwork /getImagesByMedium endpoints.

--- api/routes/gallery.js
+++ api/routes/gallery.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
+router.use(express.json())
+
 const pgp = require('pg-promise')()
 const db = pgp(process.env.DB_URL)
 
+// GET endpoints
 router.get("/", (req, res) => {
     db.any('SELECT * FROM artworks')
         .then(data => {
-            console.log('DATA:', data)
+            // console.log('DATA:', data)
             res.json({
                 message: data
             })
@@ -21,9 +24,24 @@ router.get("/getImageById", (req, res) => {
 })
 
 router.get("/getImagesByMedium", (req, res) => {
-    res.json({
-        message: "This endpoint get images by media type."
-    })
+    db.any('SELECT * FROM artworks WHERE medium = $1', [req.body.medium])
+        .then(data => {
+            // console.log('DATA:', data)
+            res.json({
+                message: data
+            })
+        })
+})
+
+// POST endpoints
+router.post("/addArtwork", (req, res) => {
+    db.none('INSERT INTO artworks(name, url, description, medium) VALUES($1, $2, $3, $4)', [req.body.name, req.body.url, req.body.description, req.body.medium])
+        .then(data => {
+            // console.log('DATA:', data)
+            res.json({
+                message: "Artwork inserted into database."
+            })
+        })
 })
 
 module.exports = router
